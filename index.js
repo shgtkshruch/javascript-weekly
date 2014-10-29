fde2cfe248c3a25952a0ec56899ae04326e8edd4
shgtkshruch <shgtk.shruch@gmail.com>
2014-10-29 12:34:32 +0900

    Add index.js

---
+++ index.js
@@ -0,0 +1,90 @@
+var http = require('http');
+var cheerio = require('cheerio');
+var async = require('async');
+
+module.exports = function (issueNumber. callback) {
+
+  function getHtml(cb) {
+    var url = 'http://javascriptweekly.com/issues/' + issueNumber;
+
+    http.get(url, function (res) {
+      var html;
+      res.on('data', function (data) {
+        html += data; 
+      });
+
+      res.on('end', function () {
+        cb(null, html);
+      });
+    });
+  }
+
+  function parseHtml(html, cb) {
+    var $ = cheerio.load(html);
+    var issue = {};
+
+    var main = $('.issue-html .container').eq(1);
+    var heading = $(main).children().eq(0).find('.gowide.lonmo span').text();
+    issue.title = heading.match(/[A-Za-z]+\s[0-9]+\s/)[0].trim();
+    issue.date = heading.match(/[A-Za-z]+\s[0-9]+\,\s[0-9]+\n$/)[0].trim();
+
+    var articles = [];
+    $(main).children().eq(1).find('table .gowide > tr > td').each(function (i, elem) {
+      var article = _getArticle($, this);
+      articles.push(article);
+    });
+    issue.articles = articles;
+
+    var brieies = [];
+    $(main).children().eq(1).find('ul').last().find('li').each(function (i, elem) {
+      var brief = _getBrief($, this);
+      brieies.push(brief);
+    });
+    issue.brieies = brieies;
+
+    cb(null, issue);
+  }
+
+  function _getArticle($, that) {
+    var article = {};
+    var source = $(that).children().eq(1).text().trim();
+
+    if (source.search(/sponsored/i) !== -1) {
+      return; 
+    }
+
+    article.title = $(that).find('a').text();
+    article.url = $(that).html().split('href="')[1].split('?')[0];
+    article.source = source;
+    article.body = $(that).children().eq(2).text().trim();
+
+    return article;
+  }
+
+  function _getBrief($, that) {
+    var brief = {};
+    brief.cats = [];
+
+    brief.title = $(that).find('a').text();
+    brief.url = $(that).html().split('href="')[1].split('?')[0];
+    brief.body = $(that).find('br').next().remove().text().trim();
+    brief.source = $(that).find('span').last().remove().text().trim();
+
+    $(that).find('span').each(function (i, elem) {
+      brief.cats.push($(this).text());
+    });
+
+    return brief;
+  }
+
+  async.waterfall([
+    function (cb) {
+      getHtml(cb);
+    },
+    function (html, cb) {
+      parseHtml(html, cb);
+    }
+  ], function (err, issue) {
+    callback(issue);
+  });
+}
